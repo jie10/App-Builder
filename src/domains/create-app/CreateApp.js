f222e35757996ac41e6e3d3fabd28d1fa1018925
Jemimah Sai <jemimah.beryl.sai@gmail.com>
2021-11-03 08:17:55 +0800

    Create new files for CreateNewFolderModal and import it to CreateApp

--- src/domains/create-app/CreateApp.js
+++ src/domains/create-app/CreateApp.js
@@ -7,12 +7,12 @@ import {
     NotificationsNoneOutlined as NotificationsIcon,
     AddOutlined as AddOutlinedIcon,
     CreateNewFolderOutlined as CreateNewFolderOutlinedIcon,
-    CloseOutlined as CloseOutlinedIcon,
-    Error as ErrorIcon,
     LanguageOutlined as LanguageOutlinedIcon
 } from '@mui/icons-material';
 
 import "./CreateApp.css";
+
+import CreateNewFolderModal from './CreateNewFolderModal';
 
 import siteLogo from "../../assets/logos/ceblogo.png";
 import sampleUserAvatar from "../../assets/images/sample-user-avatar.jpeg";
@@ -389,82 +389,6 @@ const FirstWebsiteSection = () => {
                         <p className="main-features-subtitle">You may have opened another account. <a href="/" target="_blank">Read More</a></p>
                     </Grid>
                 </Grid>
-            </div>
-        </div>
-    );
-}
-
-const CreateNewFolderModal = (props) => {
-    const { hiddenModal, setHiddenModal } = props;
-
-    const [userInput, setUserInput] = useState(null);
-
-    const handleCloseModal = (e) => {
-        e.preventDefault();
-        setUserInput(null);
-        setHiddenModal(true);
-    }
-
-    const handleUserInput = (e) => {
-        e.preventDefault();
-        setUserInput(e.target.value);
-    }
-
-    return(
-        <div className={`create-new-folder-modal-container ${ hiddenModal === true ? 'hidden' : ''}`}>
-            <div className="create-new-folder-modal-box">
-                <div className="modal-box modal-box-header">
-                    <span className="header-title">Create New Folder</span>
-                    <button
-                        className="header-close-button"
-                        onClick={handleCloseModal}>
-                            <CloseOutlinedIcon />
-                    </button>
-                </div>
-                <div className="modal-box modal-box-body">
-                    <div className="body-form-label-container">
-                        <label className="form-label">Name your folder, then move any app to this folder via Actions.</label>
-                    </div>
-                    <div className="body-form-input-container">
-                        <input
-                            className={`form-input ${ userInput === null ? '' : userInput && userInput.length > 0 ? '' : 'form-error' }`}
-                            type="text"
-                            maxLength="300"
-                            placeholder="e.g. In Progress"
-                            onChange={handleUserInput}
-                            value={ userInput ? userInput : '' } />
-                        <span
-                            className={`form-error-icon ${ userInput === null ? 'hidden' : userInput && userInput.length > 0 ? 'hidden' : ''}`}>
-                            <Tooltip
-                                TransitionComponent={Zoom}
-                                sx={{
-                                    position: "absolute",
-                                    top: "14px",
-                                    right: "8px"
-                                }}
-                                title={<span
-                                            className="form-tooltip" 
-                                            style={{ display: "block", width: "150px", fontSize: "14px", padding: "8px", wordBreak: "break-word" }}>
-                                                Folder names should be between 1 and 256 characters long.
-                                        </span>}
-                                placement="top"
-                                describeChild
-                                arrow>
-                                <ErrorIcon />
-                            </Tooltip>
-                        </span>
-                    </div>
-                </div>
-                <div className="modal-box modal-box-footer">
-                    <div className="footer-buttons-container">
-                        <button className="footer-button" onClick={handleCloseModal}>Cancel</button>
-                        <button
-                            className={`footer-button ${ userInput && userInput.length > 0 ? 'footer-main-button' : 'disabled-button' }`}
-                            disabled={ userInput && userInput.length > 0  ? false : true }>
-                                Create
-                        </button>
-                    </div>
-                </div>
             </div>
         </div>
     );
