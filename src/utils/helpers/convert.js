973a4393a48c4f2a1f60c00ab7672b8f63d2f17a
Jemimah Sai <jemimah.beryl.sai@gmail.com>
2021-11-18 08:28:21 +0800

    Fix issue: timestamp from now calculates differently as expected

--- src/utils/helpers/convert.js
+++ src/utils/helpers/convert.js
@@ -1,33 +1,9 @@
+const moment = require('moment');
+
 /** STRING CONVERSIONS */
 export const capitalizeWord = (word) => word.slice(0, 1).toUpperCase() + word.slice(1).toLowerCase();
 
 /** DATE-TIME CONVERSIONS */
-export const convertTimestampToDateTime = (unixTimestamp) => new Date(unixTimestamp);
+export const convertTimestampToDateTime = (unixTimestamp) => moment().format('llll');
 
-export const convertTimestampFromNow = (unixTimestamp) => {
-    let setDate = convertTimestampToDateTime(unixTimestamp);
-    let nowTimeStamp = new Date();
-
-    let years = Math.abs(nowTimeStamp.getFullYear() - setDate.getFullYear());
-    let months = Math.abs(nowTimeStamp.getMonth() - setDate.getMonth());
-    let days = Math.abs(nowTimeStamp.getDay() - setDate.getDay());
-    let hours = Math.abs(nowTimeStamp.getHours() - setDate.getHours());
-    let minutes = Math.abs(nowTimeStamp.getMinutes() - setDate.getMinutes());
-    let seconds = Math.abs(nowTimeStamp.getSeconds() - setDate.getSeconds());
-
-    if (years > 0) {
-        return `${years > 1 ? `${years} years` : "A year"} ago`;
-    } else if (months > 0) {
-        return `${months > 1 ? `${months} months` : "A month"} ago`;
-    } else if (days > 0) {
-        return `${days > 1 ? `${days} days` : "A day"} ago`;
-    } else if (hours > 0) {
-        return `${hours > 1 ? `${hours} hours` : "An hour"} ago`;
-    } else if (minutes > 0) {
-        return `${minutes > 1 ? `${minutes} minutes` : "A minute"} ago`;
-    } else if (seconds > 0) {
-        return "A few seconds ago";
-    } else {
-        return "Now";
-    }
-}
+export const convertTimestampFromNow = (unixTimestamp) => moment(unixTimestamp).fromNow();
