173ab4be609830486227f0a3f1e29ecc59188a6f
Jemimah Sai <jemimah.beryl.sai@gmail.com>
2021-11-18 08:29:56 +0800

    Fix issue: correct conversion of timestamp to datetime

--- src/utils/helpers/convert.js
+++ src/utils/helpers/convert.js
@@ -4,6 +4,6 @@ const moment = require('moment');
 export const capitalizeWord = (word) => word.slice(0, 1).toUpperCase() + word.slice(1).toLowerCase();
 
 /** DATE-TIME CONVERSIONS */
-export const convertTimestampToDateTime = (unixTimestamp) => moment().format('llll');
+export const convertTimestampToDateTime = (unixTimestamp) => moment(unixTimestamp).format('llll');
 
 export const convertTimestampFromNow = (unixTimestamp) => moment(unixTimestamp).fromNow();
