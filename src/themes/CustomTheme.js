732e109b290a0bfd44458e5f3be4a39cbd3ebb5a
Jemimah Sai <jemimah.beryl.sai@gmail.com>
2022-01-10 15:26:16 +0800

    Fix issue: Global Style does not apply to canvas

--- src/themes/CustomTheme.js
+++ src/themes/CustomTheme.js
@@ -1,3 +1,3 @@
 import GlobalStyles from '@mui/material/GlobalStyles';
 
-export const applyCustomTheme = (globalStyleSettings) => <GlobalStyles styles={{ body: globalStyleSettings }} />;
+export const applyCustomTheme = (globalStyleSettings) => <GlobalStyles styles={globalStyleSettings} />;
