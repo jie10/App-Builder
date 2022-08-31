156c5814e0f2c9b9bd290149fa28c5190dab9c04
Benjie Jabir Burillo <benjieburillo10@gmail.com>
2022-08-31 09:47:40 +0800

    Update Canvas.js

--- src/components/Editor/Canvas.js
+++ src/components/Editor/Canvas.js
@@ -27,11 +27,34 @@ import SearchBarSection from '../Blocks/Sections/Search'
 import FilterSection from '../Blocks/Sections/Filter'
 import SeatSaleSection from '../Blocks/Sections/SeatSale'
 import ColumnSection from '../Blocks/Column/Column'
+<<<<<<< Updated upstream
+=======
+import ToggleNavTab from '../Blocks/ToggleNavTab'
+import SplashScreen from '../Blocks/SplashScreen'
+import ListSection from '../Blocks/Sections/ListSection'
+import WideButton from '../Blocks/Sections/WideButton'
+>>>>>>> Stashed changes
 import Forms from '../Blocks/Forms'
 import './Canvas.css'
 import PinCodeInput from '../Blocks/Forms/PinCodeInput'
 import SignupForm from '../Blocks/Forms/SignupForm'
+<<<<<<< Updated upstream
 
+=======
+import BottomNavbar from '../Blocks/Navbar/Bottom-Navbar'
+import DatePicker from '../Blocks/DatePicker'
+import TimePicker from '../Blocks/TimePicker'
+import DateTimePicker from '../Blocks/DateTimePicker'
+import SingleSelectField from '../Blocks/SingleSelectField'
+import MultiSelectField from '../Blocks/MultiSelectField'
+import TextInputField from '../Blocks/TextInputField'
+import TextAreaField from '../Blocks/TextAreaField'
+import ButtonText from '../Blocks/ButtonText'
+import ButtonIcon from '../Blocks/ButtonIcon'
+import ClickableTextInputField from '../Blocks/ClickableTextInputField'
+import AddressButton from '../Blocks/AddressButton'
+import ButtonTextInputField from '../Blocks/ButtonTextInputField'
+>>>>>>> Stashed changes
 
 class Canvas extends Component {
   async componentDidMount() {
@@ -59,7 +82,10 @@ class Canvas extends Component {
           className={ `content-container ${ selectedPreviewMenu === 'tablet' ? 'content-container-tablet' : selectedPreviewMenu === 'mobile' ? 'content-container-mobile' : 'content-container-desktop' }` }>
           { blocks && Object.keys(blocks).map(key => {
             let block = blocks[key]
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
             switch (block.type) {
               case 'HEADER':
                 return <HeaderSimple key={ key } _id={ key } themeStyle={ themeStyle['.header_simple'] }
@@ -124,6 +150,7 @@ class Canvas extends Component {
               case 'FOOTER':
                 return <FooterSimple key={ key } _id={ key } themeStyle={ themeStyle['.footer_simple'] }
                                      block={ block }/>
+<<<<<<< Updated upstream
               case 'FORM_BLOCKS':
                 return <Forms key={ key } _id={ key } themeStyle={ themeStyle['.form_blocks'] } block={ block }/>
               case 'PinCode_Input':
@@ -132,6 +159,66 @@ class Canvas extends Component {
               case 'Signup_Form':
                 return <SignupForm key={ key } _id={ key } themeStyle={ themeStyle['.Signup_Form'] }
                                    block={ block }/>
+=======
+              case 'TOGGLE_NAV_TAB':
+                return <ToggleNavTab key={ key } _id={ key } themeStyle={ themeStyle['.toggle-nav-tab-component'] }
+                                     block={ block }/>
+              case 'SPLASH_SCREEN':
+                return <SplashScreen key={ key } _id={ key } themeStyle={ themeStyle['.splash-screen-component'] }
+                                     block={ block }/>
+              case 'LIST_SECTION':
+                return <ListSection key={ key } _id={ key } themeStyle={ themeStyle['.list_section'] } block={ block }/>
+              case 'WIDE_BUTTON':
+                return <WideButton key={ key } _id={ key } themeStyle={ themeStyle['.wide_button'] } block={ block }/>
+              case 'FORM_BLOCKS':
+                return <Forms key={ key } _id={ key } themeStyle={ themeStyle['.form_blocks'] } block={ block }/>
+              case 'PINCODE_INPUT':
+                return <PinCodeInput key={ key } _id={ key } themeStyle={ themeStyle['.pincode_input'] }
+                                     block={ block }/>
+              case 'Signup_Form':
+                return <SignupForm key={ key } _id={ key } themeStyle={ themeStyle['.Signup_Form'] } block={ block }/>
+              case 'BOTTOM_NAVBAR':
+                return <BottomNavbar key={ key } _id={ key } themeStyle={ themeStyle['.bottom_navbar'] }
+                                     block={ block }/>
+              case 'DATE_PICKER':
+                return <DatePicker key={ key } _id={ key } themeStyle={ themeStyle['.date-picker-component'] }
+                                   block={ block }/>
+              case 'TIME_PICKER':
+                return <TimePicker key={ key } _id={ key } themeStyle={ themeStyle['.time-picker-component'] }
+                                   block={ block }/>
+              case 'DATETIME_PICKER':
+                return <DateTimePicker key={ key } _id={ key } themeStyle={ themeStyle['.datetime-picker-component'] }
+                                       block={ block }/>
+              case 'SINGLE_SELECT':
+                return <SingleSelectField key={ key } _id={ key } themeStyle={ themeStyle['.single-select-component'] }
+                                          block={ block }/>
+              case 'MULTI_SELECT':
+                return <MultiSelectField key={ key } _id={ key } themeStyle={ themeStyle['.multi-select-component'] }
+                                         block={ block }/>
+              case 'TEXT_INPUT_FIELD':
+                return <TextInputField key={ key } _id={ key } themeStyle={ themeStyle['.text-input-field-component'] }
+                                       block={ block }/>
+              case 'TEXT_AREA_FIELD':
+                return <TextAreaField key={ key } _id={ key } themeStyle={ themeStyle['.text-area-field-component'] }
+                                      block={ block }/>
+              case 'BUTTON_TEXT':
+                return <ButtonText key={ key } _id={ key } themeStyle={ themeStyle['.button-with-text-component'] }
+                                   block={ block }/>
+              case 'BUTTON_ICON':
+                return <ButtonIcon key={ key } _id={ key } themeStyle={ themeStyle['.button-with-icon-component'] }
+                                   block={ block }/>
+              case 'CLICKABLE_TEXT_INPUT_FIELD':
+                return <ClickableTextInputField key={ key } _id={ key }
+                                                themeStyle={ themeStyle['.clickable-text-input-field-component'] }
+                                                block={ block }/>
+              case 'ADDRESS_BUTTON':
+                return <AddressButton key={ key } _id={ key } themeStyle={ themeStyle['.address-button-component'] }
+                                      block={ block }/>
+              case 'BUTTON_TEXT_INPUT_FIELD':
+                return <ButtonTextInputField key={ key } _id={ key }
+                                             themeStyle={ themeStyle['.button-text-input-field-component'] }
+                                             block={ block }/>
+>>>>>>> Stashed changes
               default:
                 return <div key={ key }>DEFAULT</div>
             }
