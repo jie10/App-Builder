24169fd678675ac89216445b2258c3b66066e49c
Jem <jemimah.beryl.sai@gmail.com>
2022-09-01 15:51:24 +0800

    Modify form builder: add signup form

--- src/components/Modals/FormBlockModal/index.js
+++ src/components/Modals/FormBlockModal/index.js
@@ -27,7 +27,7 @@ const useStyles = makeStyles(theme => ({
     },
     paper: {
         position: 'absolute',
-        width: 450,
+        width: 480,
         backgroundColor: '#ffffff',
         padding: '36px 24px'
     },
@@ -44,60 +44,24 @@ const FormBlockModal = (props) => {
 
     const handleFormBlockType = (formBlockType) => {
         switch(formBlockType) {
-            case "login_form":                         
-                break;
             case "login_pincode_form":
                 break;
             case "forgot_pincode_form":
                 break;
             case "signup_form":
                 let signup_block = {
-                    type: "Signup_Form",
+                    type: "SIGNUP_FORM",
                     status: "added",
-                    parameters: {
-                        "placeholder": "Enter your CEB Email",
-                        "placeholder2": "92xx xxx xxx",
-                        "label1": "Email",
-                        "label2": "Mobile Number",
-                        "label3": "Target Location",
-                        "label4": "No Landmark",
-                        "text1": "Your preferred pickup or drop off used in carpool matching",
-                        "text2": "Region, province, municipality, Barangay",
-                        "fontSize1": "",
-                        "fontFamily1": "",
-                        "fontWeight1": "",
-                        "color1": "",
-                        "fontSize": "",
-                        "fontFamily": "",
-                        "fontWeight": "",
-                        "color": "",
-                        "borderRadius": "",
-                        "fontSize2": "",
-                        "fontFamily2": "",
-                        "fontWeight2": "",
-                        "color2": "",
-                        "fontSize3": "",
-                        "fontFamily3": "",
-                        "fontWeight3": "",
-                        "color3": "",
-                        "btnHeight": "",
-                        "btnWidth": "",
-                        "btnColor": "",
-                        "btnRadius": "",
-                        "imgHeight": "",
-                        "imgWidth": "",
-                        "textSize1": "",
-                        "textFamily1": "",
-                        "textWeight1": "",
-                        "textcolor1": "",
-                        "textSize2": "",
-                        "textFamily2": "",
-                        "textWeight2": "",
-                        "textcolor2": "",
-                        "btnradius2": "",
-                        "btnHeight2": "",
-                        "btnWidth2": "",
-                        "btnColor2": ""
+                    "parameters": {
+                        "action": "#",
+                        "method": "",
+                        "form_id": "sign_up_form",
+                        "background_color": "#FFFFFF",
+                        "form_fields_width": "80%",
+                        "label_font_size": "1rem",
+                        "button_background_color": "rgb(13, 110, 253)",
+                        "button_text_color": "#ffffff",
+                        "button_border_radius": "24px"
                     }
                 };
 
@@ -122,13 +86,6 @@ const FormBlockModal = (props) => {
                         <p>Please select which type of form you want to add to the canvas</p>
                     </div>
                     <div className='form-options-list'>
-                        <button type='button' className='form-options' onClick={() => {
-                            handleFormBlockType("login_form");
-                            handleClose();
-                        }}>
-                            <span className='icon'><GridViewImage /></span>
-                            <span className='label'>Login</span>
-                        </button>
                         <button type='button' className='form-options' onClick={() => {
                             handleFormBlockType("login_pincode_form");
                             handleClose();
