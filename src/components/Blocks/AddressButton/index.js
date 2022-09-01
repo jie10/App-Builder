f9b293df0f1510b93d9818a35a536cfbf639ddd9
Jem <jemimah.beryl.sai@gmail.com>
2022-09-01 15:47:05 +0800

    Modify Sign Up form UI and properties - to match with Juander forms

--- src/components/Blocks/AddressButton/index.js
+++ src/components/Blocks/AddressButton/index.js
@@ -44,7 +44,9 @@ const AddressButton = (props) => {
                 </Grid>
             </Grid>
             <div id="address_button_component" className="address-button-component" style={{ backgroundColor: block.parameters.background_color }}>
-                 <button type="button" class="address-button" id="address_button" style={{ width: block.parameters.width }}>
+                <label class="address-title">Target Location</label>
+                <p class="address-title-info">Your preferred pickup or drop off used in carpool matching</p>
+                <button type="button" class="address-button" id="address_button" style={{ width: block.parameters.width }}>
                     <span class="icon">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M168.3 499.2C116.1 435 0 279.4 0 192C0 85.96 85.96 0 192 0C298 0 384 85.96 384 192C384 279.4 267 435 215.7 499.2C203.4 514.5 180.6 514.5 168.3 499.2H168.3zM192 256C227.3 256 256 227.3 256 192C256 156.7 227.3 128 192 128C156.7 128 128 156.7 128 192C128 227.3 156.7 256 192 256z"/></svg>
                     </span>
--- src/components/Blocks/Forms/SignupForm/index.js
+++ src/components/Blocks/Forms/SignupForm/index.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 import { Grid } from '@mui/material'
+import TextInputField from '../../TextInputField';
 import './style.css'
 import navigationLogo from '../../../../assets/svgs/navigation-svgrepo-com.svg'
 import {
@@ -55,78 +56,54 @@ const SignupForm = (props) => {
       </Grid>
 
       <div className={ 'container' }>
-        <form>
-          <div>
-            <label className="label1" style={ {
-              fontSize: block.parameters.fontSize,
-              fontFamily: block.parameters.fontFamily,
-              fontWeight: block.parameters.fontWeight,
-            } }>{ label1 }</label>
-            <input type={ 'text' } placeholder={ placeholder } style={ {
-              fontSize: block.parameters.input_fontSize,
-              fontFamily: block.parameters.input_fontFamily,
-              fontWeight: block.parameters.input_fontWeight,
-              backgroundColor: block.parameters.input_bgColor,
-              borderRadius: block.parameters.input_borderRadius,
-            } }/>
+        <form className='sign-up-form' id={ block.parameters.form_id } action={ block.parameters.action } method={ block.parameters.method } style={{ backgroundColor: block.parameters.background_color }}>
+          <div className='form-container' style={{ width: block.parameters.form_fields_width }}>
+            <div id="text_input_field_component" className="text-input-field-component">
+              <label htmlFor="text_input_field" style={{ fontSize: block.parameters.label_font_size }}>Email</label>
+              <input type="email"
+                      id="text_input_field"
+                      className="text-input-field"
+                      name="new_text_input_field"
+                      placeholder="Enter your CEB Email"
+                      maxLength={ 150 } readOnly />
+            </div>
+            <div id="text_input_field_component" className="text-input-field-component">
+              <label htmlFor="text_input_field" style={{ fontSize: block.parameters.label_font_size }}>Mobile Number</label>
+              <input type="email"
+                      id="text_input_field"
+                      className="text-input-field"
+                      name="new_text_input_field"
+                      placeholder="917 346 9999"
+                      maxLength={ 150 } readOnly />
+            </div>
+            <div id="address_button_component" className="address-button-component">
+                <label class="address-title">Target Location</label>
+                <p class="address-title-info">Your preferred pickup or drop off used in carpool matching</p>
+                <button type="button" class="address-button" id="address_button">
+                    <span class="icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M168.3 499.2C116.1 435 0 279.4 0 192C0 85.96 85.96 0 192 0C298 0 384 85.96 384 192C384 279.4 267 435 215.7 499.2C203.4 514.5 180.6 514.5 168.3 499.2H168.3zM192 256C227.3 256 256 227.3 256 192C256 156.7 227.3 128 192 128C156.7 128 128 156.7 128 192C128 227.3 156.7 256 192 256z"/></svg>
+                    </span>
+                    <span class="details">
+                        <span class="landmark-label" id="landmark_label">No Landmark</span>
+                        <span class="address-label" id="address_label">Region, province, municipality, barangay</span>
+                    </span>
+                    <span class="icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M438.6 278.6l-160 160C272.4 444.9 264.2 448 256 448s-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L338.8 288H32C14.33 288 .0016 273.7 .0016 256S14.33 224 32 224h306.8l-105.4-105.4c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l160 160C451.1 245.9 451.1 266.1 438.6 278.6z"/></svg>
+                    </span>
+                </button>
+            </div>
+            <div id="button_text_component" className="button-with-text-component" >
+                  <button type="button"
+                          id="button_text"
+                          className="button-text"
+                          style={{ backgroundColor: block.parameters.button_background_color,
+                                    color: block.parameters.button_text_color,
+                                    borderRadius: block.parameters.button_border_radius }}
+                          disabled>
+                      Submit
+                  </button>
+            </div>
           </div>
-          <div>
-            <label className="label2" style={ {
-              fontSize: block.parameters.fontSize,
-              fontFamily: block.parameters.fontFamily,
-              fontWeight: block.parameters.fontWeight,
-              color: block.parameters.color,
-            } }>{ label2 }</label>
-            <input type={ 'text' } placeholder={ placeholder2 } style={ {
-              fontSize: block.parameters.input_fontSize,
-              fontFamily: block.parameters.input_fontFamily,
-              fontWeight: block.parameters.input_fontWeight,
-              backgroundColor: block.parameters.input_bgColor,
-              borderRadius: block.parameters.input_borderRadius,
-            } }/>
-          </div>
-          <div>
-            <label className="label3" style={ {
-              fontSize: block.parameters.fontSize,
-              fontFamily: block.parameters.fontFamily,
-              fontWeight: block.parameters.fontWeight,
-            } }>{ label3 }</label>
-            <p style={ {
-              fontSize: block.parameters.text1_Size,
-              fontWeight: block.parameters.text1_textWeight,
-            } }>{ text1 }</p>
-            <button className={ 'button' } style={ {
-              height: block.parameters.btn1_Height,
-              width: block.parameters.btn1_btnWidth,
-              backgroundColor: block.parameters.btn1_btnColor,
-              borderRadius: block.parameters.btn1_btnRadius,
-            } }>
-              <div className={ 'columns' }>
-                <div className={ 'column' }>
-                  <img style={ {height: block.parameters.imgHeight, width: block.parameters.imgWidth} }
-                       src={ navigationLogo }/>
-                </div>
-                <div className={ 'column' }>
-                  <label style={ {
-                    fontSize: block.parameters.label4_Size,
-                    fontFamily: block.parameters.label4_fontFamily,
-                    fontWeight: block.parameters.label4_fontWeight,
-                  } }>{ label4 }</label>
-                  <p style={ {
-                    fontSize: block.parameters.text2_Size,
-                    fontWeight: block.parameters.text2_textWeight,
-                  } }>{ text2 }</p>
-                </div>
-              </div>
-            </button>
-          </div>
-          <button className={ 'button is-info m-1' } style={ {
-            height: block.parameters.btn2_Height,
-            width: block.parameters.btn2_btnWidth,
-            backgroundColor: block.parameters.btn2_btnColor,
-            borderRadius: block.parameters.btn2_btnRadius,
-          } }>Sign Up
-          </button>
         </form>
       </div>
 
