d937b908ae689c2cc5431e71c39a7c55172d5219
jie10 <benjieburillo10@gmail.com>
2022-09-02 08:55:46 +0800

    update ProfileStats

--- src/components/Blocks/Profile/ProfileStats/index.js
+++ src/components/Blocks/Profile/ProfileStats/index.js
@@ -2,10 +2,6 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { Grid } from '@mui/material'
 import './style.css'
-import star from './svg/star_FILL1_wght400_GRAD0_opsz48.svg'
-import flight from './svg/flight_FILL1_wght400_GRAD0_opsz48.svg'
-import badge from './svg/military_tech_FILL1_wght400_GRAD0_opsz48.svg'
-import awesome from './svg/auto_awesome_FILL1_wght400_GRAD0_opsz48.svg'
 
 import {
   deleteBlock,
@@ -16,6 +12,11 @@ import {
 
 const ProfileStats = (props) => {
   const {_id, block, deleteBlock, moveUpBlock, moveDownBlock, getBlock, themeStyle} = props
+
+  const label1_text = block.parameters.label1_text
+  const label2_text = block.parameters.label2_text
+  const label3_text = block.parameters.label3_text
+  const label4_text = block.parameters.label4_text
 
   if (block.status === 'added') {
     block.parameters = themeStyle
@@ -49,14 +50,18 @@ const ProfileStats = (props) => {
           </Grid>
         </Grid>
 
-        <div className={ 'container' }>
+        <div className={ 'container center-container' }>
           <div className={ 'columns' }>
             <div className={ 'column' }>
               <div className={ 'columns' }>
-                <div className={ 'column' }><img className={ 'statsicons' } alt={ star } src={ star }/></div>
                 <div className={ 'column' }>
-                  <h1 className={ 'points' } style={ {fontWeight: block.parameters.fontWeight, fontSize: block.parameters.fontSize, color: block.parameters.fontColor} }>0</h1>
-                  <span>Points</span>
+                  <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24" style={ {fill: block.parameters.icon_color} }>
+                    <path d="m5.825 22 1.625-7.025L2 10.25l7.2-.625L12 3l2.8 6.625 7.2.625-5.45 4.725L18.175 22 12 18.275Z"/>
+                  </svg>
+                </div>
+                <div className={ 'column' }>
+                  <p style={ {fontWeight: block.parameters.points_fontWeight, fontSize: block.parameters.points_fontSize, color: block.parameters.points_fontColor} }>0</p>
+                  <span style={ {fontWeight: block.parameters.label_weight, fontSize: block.parameters.label_size} }>{ label1_text }</span>
                 </div>
               </div>
             </div>
@@ -64,34 +69,49 @@ const ProfileStats = (props) => {
 
             <div className={ 'column' }>
               <div className={ 'columns' }>
-                <div className={ 'column' }><img className={ 'statsicons' } alt={ flight } src={ flight }/></div>
+                <div className={ 'column' }><a href="#">
+                  <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24" style={ {fill: block.parameters.icon_color} }>
+                    <path d="M8.5 22v-1.5l2-1.5v-5.5L2 16v-2l8.5-5V3.5q0-.625.438-1.062Q11.375 2 12 2t1.062.438q.438.437.438 1.062V9l8.5 5v2l-8.5-2.5V19l2 1.5V22L12 21Z"/>
+                  </svg>
+                </a></div>
                 <div className={ 'column' }>
-                  <h1 className={ 'points' } style={ {fontWeight: block.parameters.fontWeight, fontSize: block.parameters.fontSize, color: block.parameters.fontColor} }>0</h1>
-                  <span>DTP</span>
+                  <p style={ {fontWeight: block.parameters.points_fontWeight, fontSize: block.parameters.points_fontSize, color: block.parameters.points_fontColor} }>0</p>
+                  <span style={ {fontWeight: block.parameters.label_weight, fontSize: block.parameters.label_size} }>{ label2_text }</span>
                 </div>
               </div>
             </div>
 
             <div className={ 'column' }>
               <div className={ 'columns' }>
-                <div className={ 'column' }><img className={ 'statsicons' } alt={ badge } src={ badge }/></div>
                 <div className={ 'column' }>
-                  <h1 className={ 'points' } style={ {fontWeight: block.parameters.fontWeight, fontSize: block.parameters.fontSize, color: block.parameters.fontColor} }>0</h1>
-                  <span>Badges</span>
+                  <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24" style={ {fill: block.parameters.icon_color} }>
+                    <path
+                      d="M7 2h10v7.85q0 .575-.25 1.025-.25.45-.7.725l-3.55 2.1.7 2.3H17l-3.1 2.2 1.2 3.8-3.1-2.35L8.9 22l1.2-3.8L7 16h3.8l.7-2.3-3.55-2.1q-.45-.275-.7-.725Q7 10.425 7 9.85Zm4 2v7.05l1 .6 1-.6V4Z"/>
+                  </svg>
+                </div>
+                <div className={ 'column' }>
+                  <p style={ {fontWeight: block.parameters.points_fontWeight, fontSize: block.parameters.points_fontSize, color: block.parameters.points_fontColor} }>0</p>
+                  <span style={ {fontWeight: block.parameters.label_weight, fontSize: block.parameters.label_size} }>{ label3_text }</span>
                 </div>
               </div>
             </div>
 
             <div className={ 'column' }>
               <div className={ 'columns' }>
-                <div className={ 'column' }><img className={ 'statsicons' } alt={ awesome } src={ awesome }/></div>
                 <div className={ 'column' }>
-                  <h1 className={ 'points' } style={ {fontWeight: block.parameters.fontWeight, fontSize: block.parameters.fontSize, color: block.parameters.fontColor} }>0</h1>
-                  <span>Items</span>
+                  <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24" style={ {fill: block.parameters.icon_color} }>
+                    <path
+                      d="m19 9-1.25-2.75L15 5l2.75-1.25L19 1l1.25 2.75L23 5l-2.75 1.25Zm0 14-1.25-2.75L15 19l2.75-1.25L19 15l1.25 2.75L23 19l-2.75 1.25ZM9 20l-2.5-5.5L1 12l5.5-2.5L9 4l2.5 5.5L17 12l-5.5 2.5Z"/>
+                  </svg>
+                </div>
+                <div className={ 'column' }>
+                  <p style={ {fontWeight: block.parameters.points_fontWeight, fontSize: block.parameters.points_fontSize, color: block.parameters.points_fontColor} }>0</p>
+                  <span style={ {fontWeight: block.parameters.label_weight, fontSize: block.parameters.label_size} }>{ label4_text }</span>
                 </div>
               </div>
             </div>
           </div>
+
         </div>
 
       </div>
