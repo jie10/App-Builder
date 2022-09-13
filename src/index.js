838c2995b2a5b3c15f202bf0721604f90facbfcf
jie10 <benjieburillo10@gmail.com>
2022-09-13 13:12:52 +0800

    update main.js import swiper

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux'
 import { createStore, applyMiddleware, compose } from 'redux'
 import reduxThink from 'redux-thunk'
 import './assets/style/custom/css/bulma.css'
+import 'swiper/swiper-bundle.css'
 import './index.css'
 import App from './components/App/App'
 import reducers from '../src/stores/reducers'
@@ -14,14 +15,14 @@ const store = createStore(reducers, composeEnhancers(applyMiddleware(reduxThink)
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
+    <Provider store={ store }>
+      <App/>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
-);
+  document.getElementById('root'),
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
